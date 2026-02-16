94497dd1b96f1acfd30bbde390eace92d86fc173
Toby Chen <95775315+ToadBoyChen@users.noreply.github.com>
2026-02-16 19:50:53 +0000

    bendy line

--- components/MenuEntry.tsx
+++ components/MenuEntry.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useSpring, animated } from "@react-spring/web";
+import { useSpring, animated, to } from "@react-spring/web";
 
 interface MenuEntryProps {
     text: string;
@@ -9,33 +9,48 @@ interface MenuEntryProps {
 }
 
 export default function MenuEntry(props: MenuEntryProps) {
-    const [springs, api] = useSpring(() => ({
-        x: 0,
-        config: { tension: 300, friction: 20 },
+    const [{ x, y }, underlineApi] = useSpring(() => ({
+        x: 50,
+        y: 10,
+        config: { tension: 200, friction: 20 }
     }))
 
-    const menuEntry_enter = () => {
-        api.start({ x: 20 });
+    const handleMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        
+        const xPos = ((e.clientX - rect.left) / rect.width) * 100;
+        const yPos = ((e.clientY - rect.top) / rect.height) * 20;
+
+        underlineApi.start({ x: xPos, y: yPos });
     };
 
-    const menuEntry_leave = () => {
-        api.start({ x: 0 });
+    const handleLeave = () => {
+        underlineApi.start({ x: 50, y: 10 });
     };
 
     return (
-        <div
-            className="text-5xl border-b-2 font-black tracking-wide"
+        <animated.div
+            onMouseMove={handleMove}
+            onMouseLeave={handleLeave}
+            className="relative inline-block w-full group"
         >
-            <animated.div
-                onMouseEnter={menuEntry_enter}
-                onMouseLeave={menuEntry_leave}
-                style={springs}>
-                <Link
-                    href={props.link}
-                >
-                    {props.text}
-                </Link>
-            </animated.div>
-        </div>
+            <Link
+                href={props.link}
+                className="text-5xl font-black tracking-wide"
+            >
+                {props.text}
+            </Link>
+            <svg
+                className="w-full pointer-events-none overflow-visible -translate-y-8 sm:-translate-y-12"
+                viewBox="0 0 100 10"
+            >
+                <animated.path
+                    fill="none"
+                    stroke="black"
+                    strokeWidth="1"
+                    d={to([x,y], (mx, my) => `M 0 10 Q ${mx} ${my} 100 10`)}
+                />
+            </svg>
+        </animated.div>
     );
 }
